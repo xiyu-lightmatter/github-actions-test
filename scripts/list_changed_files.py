fbb6c917f4da1891e47219a538a7690b8f833407
Xiyu Wang <xiyu@lightmatter.co>
2024-01-04 01:13:47 +0000

    [GitHub Actions] Update the script to add a origin/ before the base ref.

--- scripts/list_changed_files.py
+++ scripts/list_changed_files.py
@@ -2,26 +2,32 @@
 # changed in the current commit. It is used to determine whether to run the
 # tests or not.
 
+import argparse
 import os
-import sys
 import subprocess
+
+
+def parse_args():
+    """Parse command-line arguments using argparse."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from_branch")
+    parser.add_argument("to_branch")
+    return parser.parse_args()
 
 
 def list_changed_files(from_branch, to_branch):
     """Return a list of files that have changed between two branches."""
-    cmd = ["git", "diff", "--name-only", from_branch, "--", to_branch]
+    cmd = ["git", "diff", "--name-only", from_branch, f'origin/{to_branch}']
     output = subprocess.check_output(cmd, universal_newlines=True)
     return output.splitlines()
 
 
 def main():
     """Print the changed files to stdout."""
-    to_branch = os.environ["GITHUB_BASE_REF"]
-    from_branch = os.environ["GITHUB_HEAD_REF"]
-    changed_files = list_changed_files(from_branch, to_branch)
-    print("Changed files:")
-    for file in changed_files:
-        print(file)
+    args = parse_args()
+    changed_files = list_changed_files(args.from_branch, args.to_branch)
+    for f in changed_files:
+        print(f)
 
 
 if __name__ == "__main__":
